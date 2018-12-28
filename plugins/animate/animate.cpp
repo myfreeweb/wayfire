f4c146d74d10f2e7d19b680698d5bc3827c3f0f2
Ilia Bozhinov <ammen99@gmail.com>
2018-12-28 10:25:39 +0200

    animate: yet another refactor

--- plugins/animate/animate.cpp
+++ plugins/animate/animate.cpp
@@ -40,20 +40,20 @@ struct animation_hook
         view->damage();
 
         if (!result)
-            finalize();
+            finalize(false);
     };
 
     signal_callback_t view_removed = [=] (signal_data *data)
     {
         if (get_signaled_view(data) == view && type != ANIMATION_TYPE_UNMAP)
-            finalize();
+            finalize(true);
     };
 
     animation_hook(wayfire_view view, wf_option duration, wf_animation_type type)
     {
         this->type = type;
         this->view = view;
-        output = view->get_output();
+        this->output = view->get_output();
 
         if (type == ANIMATION_TYPE_UNMAP)
         {
@@ -71,7 +71,7 @@ struct animation_hook
             output->connect_signal("view-disappeared", &view_removed);
     }
 
-    void finalize()
+    void finalize(bool forced)
     {
         output->render->rem_effect(&update_animation_hook, WF_OUTPUT_EFFECT_PRE);
 
@@ -82,7 +82,7 @@ struct animation_hook
         if (type == ANIMATION_TYPE_UNMAP)
             view->dec_keep_count();
 
-        if (type == ANIMATION_TYPE_MINIMIZE)
+        if (type == ANIMATION_TYPE_MINIMIZE && !forced)
             view->set_minimized(true);
 
         wl_event_loop_add_idle(core->ev_loop, delete_hook_idle<animation_type>, this);
@@ -92,9 +92,8 @@ struct animation_hook
     { }
 };
 
-class wayfire_animation : public wayfire_plugin_t {
-    signal_callback_t map_cb, unmap_cb, wake_cb;
-
+class wayfire_animation : public wayfire_plugin_t
+{
     wf_option open_animation, close_animation;
     wf_option duration, startup_duration;
 
@@ -115,24 +114,13 @@ class wayfire_animation : public wayfire_plugin_t {
         FireAnimation::fire_particle_size =
             section->get_option("fire_particle_size", "16");
 
-        using namespace std::placeholders;
-        map_cb = std::bind(std::mem_fn(&wayfire_animation::view_mapped),
-                this, _1);
-        unmap_cb = std::bind(std::mem_fn(&wayfire_animation::view_unmapped),
-                this, _1);
-
-        wake_cb = [=] (signal_data *data)
-        {
-            new wf_system_fade(output, startup_duration);
-        };
-
-        output->connect_signal("map-view", &map_cb);
-        output->connect_signal("unmap-view", &unmap_cb);
-        output->connect_signal("start-rendering", &wake_cb);
+        output->connect_signal("map-view", &on_view_mapped);
+        output->connect_signal("unmap-view", &on_view_unmapped);
+        output->connect_signal("start-rendering", &on_render_start);
     }
 
     /* TODO: enhance - add more animations */
-    void view_mapped(signal_data *ddata)
+    signal_callback_t on_view_mapped = [=] (signal_data *ddata) -> void
     {
         auto view = get_signaled_view(ddata);
         assert(view);
@@ -147,9 +135,9 @@ class wayfire_animation : public wayfire_plugin_t {
             new animation_hook<zoom_animation>(view, duration, ANIMATION_TYPE_MAP);
         else if (open_animation->as_string() == "fire")
             new animation_hook<FireAnimation>(view, duration, ANIMATION_TYPE_MAP);
-    }
+    };
 
-    void view_unmapped(signal_data *data)
+    signal_callback_t on_view_unmapped = [=] (signal_data *data) -> void
     {
         auto view = get_signaled_view(data);
 
@@ -162,13 +150,18 @@ class wayfire_animation : public wayfire_plugin_t {
             new animation_hook<zoom_animation> (view, duration, ANIMATION_TYPE_UNMAP);
         else if (close_animation->as_string() == "fire")
             new animation_hook<FireAnimation> (view, duration, ANIMATION_TYPE_UNMAP);
-    }
+    };
+
+    signal_callback_t on_render_start = [=] (signal_data *data) -> void
+    {
+        new wf_system_fade(output, startup_duration);
+    };
 
     void fini()
     {
-        output->disconnect_signal("map-view", &map_cb);
-        output->disconnect_signal("unmap-view", &unmap_cb);
-        output->disconnect_signal("start-rendering", &wake_cb);
+        output->disconnect_signal("map-view", &on_view_mapped);
+        output->disconnect_signal("unmap-view", &on_view_unmapped);
+        output->disconnect_signal("start-rendering", &on_render_start);
     }
 };
 
